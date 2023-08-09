7e89299f82ae78165c32358b9603c7d8a77d8c7d
Daniel <kuruchbekov2006dan@gmail.com>
2023-08-09 18:38:41 +0600

    adding base url

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
+import App from './App'
 import './index.css'
 import store from './redux/reduxStore'
-import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
-	<BrowserRouter>
+	<BrowserRouter basename={import.meta.env.BASE_URL}>
 		<Provider store={store}>
 			<App />
 		</Provider>
