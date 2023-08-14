e540cfe396a8a9ae34744f4f955fc81dbe86c092
Daniel <kuruchbekov2006dan@gmail.com>
2023-08-14 23:09:45 +0600

    switching to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,20 @@
-import { useEffect } from 'react'
-import { Provider, connect } from 'react-redux'
-import {
-	BrowserRouter,
-	Navigate,
-	Route,
-	Routes,
-	useLocation,
-} from 'react-router-dom'
+import { Suspense, lazy, useEffect } from 'react'
+import { connect } from 'react-redux'
+import { Navigate, Route, Routes, useLocation } from 'react-router-dom'
 import './App.css'
-import DialogsContainer from './components/Dialogs/DialogsContainer'
 import HeaderContainer from './components/Header/HeaderContainer'
 import Login from './components/Login/Login'
 import NavbarContainer from './components/Navbar/NavbarContainer'
 import NotFound from './components/NotFound/NotFound'
 import Preloader from './components/Preloader/Preloader'
-import Profile from './components/Profile/Profile'
 import ScrollToTop from './components/ScrollToTop'
-import UsersContainer from './components/Users/UsersContainer'
 import { initializeApp } from './redux/reducers/appReducer'
-import store from './redux/reduxStore'
+
+const Profile = lazy(() => import('./components/Profile/Profile'))
+const DialogsContainer = lazy(() =>
+	import('./components/Dialogs/DialogsContainer'),
+)
+const UsersContainer = lazy(() => import('./components/Users/UsersContainer'))
 
 function App({ initializeApp, initialized }) {
 	const { pathname } = useLocation()
@@ -33,15 +29,17 @@ function App({ initializeApp, initialized }) {
 			<div className='app-content-wrapper'>
 				{pathname === '/' && <Navigate to={'/profile'} replace={true} />}
 				<ScrollToTop />
-				<Routes path='/'>
-					<Route path='profile/*' element={<Profile />} />
-					<Route path='dialogs/:id' element={<DialogsContainer />} />
-					<Route path='users/' element={<UsersContainer />} />
-					<Route path='news/' element={<NotFound />} />
-					<Route path='music/' element={<NotFound />} />
-					<Route path='settings/' element={<NotFound />} />
-					<Route path='login/' element={<Login />} />
-				</Routes>
+				<Suspense fallback={<Preloader />}>
+					<Routes path='/'>
+						<Route path='profile/*' element={<Profile />} />
+						<Route path='dialogs/:id' element={<DialogsContainer />} />
+						<Route path='users/' element={<UsersContainer />} />
+						<Route path='news/' element={<NotFound />} />
+						<Route path='music/' element={<NotFound />} />
+						<Route path='settings/' element={<NotFound />} />
+						<Route path='login/' element={<Login />} />
+					</Routes>
+				</Suspense>
 			</div>
 		</div>
 	)
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 import store from './redux/reduxStore'
@@ -9,9 +9,9 @@ import store from './redux/reduxStore'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
-	<BrowserRouter basename={import.meta.env.BASE_URL}>
+	<HashRouter>
 		<Provider store={store}>
 			<App />
 		</Provider>
-	</BrowserRouter>,
+	</HashRouter>,
 )
